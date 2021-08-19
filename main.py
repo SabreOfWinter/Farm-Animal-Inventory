14cda17f37c3dfaaa8aa02d684c6938845cdd424
SabreOfWinter <steadmanbailey@gmail.com>
2021-08-19 23:34:30 +0100

    Set window position to centre

--- main.py
+++ main.py
@@ -14,6 +14,13 @@ class App(tk.Tk):
         self.tk.call("source", "sun-valley.tcl")
         self.tk.call("set_theme", "dark")
 
+        # Set a minsize for the window, and place it in the middle
+        self.update()
+        self.minsize(self.winfo_width(), self.winfo_height())
+        x_cordinate = int((self.winfo_screenwidth() / 2) - (self.winfo_width() / 2))
+        y_cordinate = int((self.winfo_screenheight() / 2) - (self.winfo_height() / 2))
+        self.geometry("+{}+{}".format(x_cordinate, y_cordinate))
+
 if __name__ == "__main__":
     app = App()
     app.mainloop()
