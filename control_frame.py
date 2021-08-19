213fb4ca02cb5adf9008af960153ec50ef69367b
SabreOfWinter <steadmanbailey@gmail.com>
2021-08-20 00:24:25 +0100

    Add variables for pages

--- control_frame.py
+++ control_frame.py
@@ -7,3 +7,11 @@ class ControlFrame(ttk.LabelFrame):
     def __init__(self, container):
 
         super().__init__(container)
+
+        # Create value lists
+        self.option_menu_list = ["", "Details", "Medical", "Accounts"]
+        self.animal_option_menu_list = ["", "All" "Sheep", "Pigs", "Geese"]
+
+        # Create control variables
+        self.selected_type = tk.StringVar(value=self.option_menu_list[1])
+        self.selected_animal = tk.StringVar(value=self.animal_option_menu_list[1])
