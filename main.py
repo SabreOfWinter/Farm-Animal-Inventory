1e8e8019aa95ea1ae7c347c9c84dd59a6299c4f0
SabreOfWinter <steadmanbailey@gmail.com>
2021-08-19 23:31:23 +0100

    Init app window
    Initial structure for running the application window.

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import *
 from tkinter.messagebox import showerror
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title('Farm Animal Inventory')
+        self.resizable(False, False)
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
